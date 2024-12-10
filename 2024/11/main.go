5f2f4427c3dd9cb6bfaffeeaec38d7c4cd919195
Jason <jason.goemaat@gmail.com>
2024-12-10 14:00:12 -0600

    2015 day 8 done, template for 2024 day 11 in place

---
+++ 2024/11/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	aoc "github.com/JasonGoemaat/go-aoc/aoc"
+)
+
+func main() {
+	// https://adventofcode.com/2015/day/X
+	aoc.Local(part1, "part1", "sample.txt", 0)
+	aoc.Local(part1, "part1", "input.txt", 0)
+	// aoc.Local(part2, "part2", "sample.txt", 0)
+	// aoc.Local(part2, "part2", "input.txt", 0)
+}
+
+func part1(contents string) interface{} {
+	return 0
+}
+
+func part2(contents string) interface{} {
+	return 0
+}
